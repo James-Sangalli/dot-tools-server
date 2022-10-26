6a0ce2497fedfa8989e83a9e0a6bea93ccdd5076
James Sangalli <j.l.sangalli@gmail.com>
2022-10-27 00:07:37 +1100

    use max length

--- migrations/20221026041658_create.js
+++ migrations/20221026041658_create.js
@@ -6,15 +6,15 @@ exports.up = function(knex) {
     return Promise.all([
         knex.schema.createTableIfNotExists('poolSelectorResultsTable', function(table) {
             table.increments('id')
-            table.string("results")
+            table.string("results", 1.7976931348623157e+308)
         }),
         knex.schema.createTableIfNotExists('dotSelectorResultsTable', function(table) {
             table.increments('id')
-            table.string("results")
+            table.string("results", 1.7976931348623157e+308)
         }),
         knex.schema.createTableIfNotExists('dotStakingPricesTable', function(table) {
             table.increments('id')
-            table.string("prices")
+            table.string("prices", 1.7976931348623157e+308)
         }),
     ])
 };
