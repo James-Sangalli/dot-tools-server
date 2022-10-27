bd9b1a4011f3af4a28f2c3fce05224c40c0c1835
James Sangalli <j.l.sangalli@gmail.com>
2022-10-27 14:43:39 +1100

    allow CORS

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ const knexConfig = require('./knexfile.js');
 const knex = require('knex')(knexConfig[process.env.NODE_ENV || "development"]);
 
 app.get('/dot-pool-selector', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   knex("poolSelectorResultsTable").select("results").then((data) => {
     res.send(data[data.length - 1].results);
   });
 });
 
 app.get('/dot-validator-selector', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   knex("dotSelectorResultsTable").select("results").then((data) => {
     res.send(data[data.length - 1].results);
   });
 });
 
 app.get('/dot-staking-income/prices', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   knex("dotStakingPricesTable").select("prices").then((data) => {
     res.send(data[data.length - 1].prices);
   });
