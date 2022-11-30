8616c1103e1b276856be4235709ff31a93c778ff
James Sangalli <j.l.sangalli@gmail.com>
2022-12-01 10:36:45 +1100

    add exception handling to routes

--- app.js
+++ app.js
@@ -7,31 +7,44 @@ const knex = require('knex')(knexConfig[process.env.NODE_ENV || "development"]);
 app.get('/dot-pool-selector/:network', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const network = req.params.network;
-  if(network == "ksm") {
-    knex("poolSelectorResultsTableKSM").select("results").then((data) => {
-      console.log(data);
-      res.send(data[data.length - 1].results);
-    });
-  } else {
-    knex("poolSelectorResultsTableDOT").select("results").then((data) => {
-      console.log(data);
-      res.send(data[data.length - 1].results);
-    });
+  try {
+    if(network == "ksm") {
+      knex("poolSelectorResultsTableKSM").select("results").then((data) => {
+        console.log(data);
+        res.send(data[data.length - 1].results);
+      });
+    } else {
+      knex("poolSelectorResultsTableDOT").select("results").then((data) => {
+        console.log(data);
+        res.send(data[data.length - 1].results);
+      });
+    }
+  } catch (e) {
+    res.send(e);
   }
+
 });
 
 app.get('/dot-validator-selector', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  knex("dotSelectorResultsTable").select("results").then((data) => {
-    res.send(data[data.length - 1].results);
-  });
+  try {
+    knex("dotSelectorResultsTable").select("results").then((data) => {
+      res.send(data[data.length - 1].results);
+    });
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.get('/dot-staking-income/prices', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  knex("dotStakingPricesTable").select("prices").then((data) => {
-    res.send(data[data.length - 1].prices);
-  });
+  try {
+    knex("dotStakingPricesTable").select("prices").then((data) => {
+      res.send(data[data.length - 1].prices);
+    });
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.listen(port, () => {
